40732dcf06a773520063101f21fd17a4ce87788c
Dhyan M Patel <dhyan.m.patel2021@gmail.com>
2024-07-26 12:22:52 +0530

    create login where PW & email Required, make response protected in Backend

--- back-end/index.js
+++ back-end/index.js
@@ -7,12 +7,28 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post('/signup',async(req,res)=>{
-    const user = new User(req.body);
+app.post('/signup', async (req, res) => {
+    const user = new User(req.body);        // take value from /signup page
     const result = await user.save();
+
+    result = result.toObject();         // convert to object
+    delete result.password;             // delete PW from object for response only
+    
     res.send(result);
 })
 
-app.listen(5000,()=>{
+app.post('/login', async (req, res) => {
+
+    if (req.body.password && req.body.email) {          /// if password & email will not give then not work
+
+        const user = await User.findOne(req.body).select("-password")        // if we pass only name, provide all details with PW (If .select() not)
+        { user ? res.send(user) : res.send("Please Enter Correct Details") }
+
+    } else {
+        res.send("All Details are not Filled")
+    }
+})
+
+app.listen(5000, () => {
     console.log('server is running');
 });
